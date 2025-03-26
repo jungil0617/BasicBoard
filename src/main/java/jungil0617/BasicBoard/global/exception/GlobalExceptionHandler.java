1ee5b3884707a4467ec0387cb09dcaded5ca2f60
jungil0617 <jungil0617@naver.com>
2025-03-27 02:20:45 +0900

    Feat: 전역 예외 처리 기능 추가

---
+++ src/main/java/jungil0617/BasicBoard/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package jungil0617.BasicBoard.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+}
