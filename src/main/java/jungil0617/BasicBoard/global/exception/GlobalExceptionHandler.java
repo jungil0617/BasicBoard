0b94b6c44f99512c63bc2e03d251e00502fe73a3
jungil0617 <jungil0617@naver.com>
2025-04-03 05:35:24 +0900

    Chore: 전역 예외 처리기 정리

--- src/main/java/jungil0617/BasicBoard/global/exception/GlobalExceptionHandler.java
+++ src/main/java/jungil0617/BasicBoard/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package jungil0617.BasicBoard.global.exception;
 
+import jungil0617.BasicBoard.comment.exception.CommentException;
+import jungil0617.BasicBoard.post.exception.PostException;
+import jungil0617.BasicBoard.user.exception.UserException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,15 +12,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<String> handleUserException(UserException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(PostException.class)
+    public ResponseEntity<String> handlePostException(PostException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CommentException.class)
+    public ResponseEntity<String> handleCommentException(CommentException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
 }
