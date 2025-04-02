d357b1897c86fe4d212eff33c30ef6996c732237
jungil0617 <jungil0617@naver.com>
2025-04-03 05:28:51 +0900

    Refactor: 저장 예외 처리 추가

--- src/main/java/jungil0617/BasicBoard/saves/service/SaveService.java
+++ src/main/java/jungil0617/BasicBoard/saves/service/SaveService.java
@@ -2,11 +2,11 @@ package jungil0617.BasicBoard.saves.service;
 
 import jungil0617.BasicBoard.post.dto.PostListResponseDto;
 import jungil0617.BasicBoard.post.entity.Post;
-import jungil0617.BasicBoard.post.repository.PostRepository;
+import jungil0617.BasicBoard.post.exception.PostValidator;
 import jungil0617.BasicBoard.saves.entity.Save;
 import jungil0617.BasicBoard.saves.repository.SaveRepository;
 import jungil0617.BasicBoard.user.entity.User;
-import jungil0617.BasicBoard.user.repository.UserRepository;
+import jungil0617.BasicBoard.user.exception.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,21 +18,15 @@ import java.util.List;
 public class SaveService {
 
     private final SaveRepository saveRepository;
-    private final UserRepository userRepository;
-    private final PostRepository postRepository;
+    private final UserValidator userValidator;
+    private final PostValidator postValidator;
 
     // 저장 토글
     @Transactional
     public boolean save(String username, Long postId) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        User user = userValidator.validateUserExists(username);
+        Post post = postValidator.validatePostExists(postId);
 
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-
-        // 나눠주기 하는 일이 많다
-        // 예외 처리 되나? 정확하게
-        // 게시글 조회까지 하기
         return saveRepository.findByUserAndPost(user, post)
                 .map(save -> {saveRepository.delete(save); return false; })
                 .orElseGet(() -> { saveRepository.save(new Save(user, post)); return true; });
@@ -40,8 +34,7 @@ public class SaveService {
 
     @Transactional(readOnly = true)
     public List<PostListResponseDto> getSavedPosts(String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        User user = userValidator.validateUserExists(username);
 
         List<Save> saves = saveRepository.findAllByUser(user);
 
