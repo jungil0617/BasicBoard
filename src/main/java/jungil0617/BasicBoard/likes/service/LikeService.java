8c91faa1fc635cd590f8a9f202f59af51af43519
jungil0617 <jungil0617@naver.com>
2025-04-03 05:31:31 +0900

    Refactor: 좋아요 예외 처리 추가

--- src/main/java/jungil0617/BasicBoard/likes/service/LikeService.java
+++ src/main/java/jungil0617/BasicBoard/likes/service/LikeService.java
@@ -3,9 +3,9 @@ package jungil0617.BasicBoard.likes.service;
 import jungil0617.BasicBoard.likes.entity.Like;
 import jungil0617.BasicBoard.likes.repository.LikeRepository;
 import jungil0617.BasicBoard.post.entity.Post;
-import jungil0617.BasicBoard.post.repository.PostRepository;
+import jungil0617.BasicBoard.post.exception.PostValidator;
 import jungil0617.BasicBoard.user.entity.User;
-import jungil0617.BasicBoard.user.repository.UserRepository;
+import jungil0617.BasicBoard.user.exception.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,17 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class LikeService {
 
     private final LikeRepository likeRepository;
-    private final UserRepository userRepository;
-    private final PostRepository postRepository;
+    private final UserValidator userValidator;
+    private final PostValidator postValidator;
 
     // 좋아요 토글
     @Transactional
     public boolean like(String username, Long postId) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
-
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        User user = userValidator.validateUserExists(username);
+        Post post = postValidator.validatePostExists(postId);
 
         return likeRepository.findByUserAndPost(user, post)
                 .map(like -> {likeRepository.delete(like); return false;})
@@ -34,8 +31,7 @@ public class LikeService {
 
     @Transactional(readOnly = true)
     public Long getLikeCount(Long postId) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        Post post = postValidator.validatePostExists(postId);
         return likeRepository.countByPost(post);
     }
 
